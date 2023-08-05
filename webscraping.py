989090da6c3f13aeeb1fe221563bcef94eb03fd2
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-08-04 23:31:16 -0300

    traversing through all homepage pages, finding some stuff on the HTML

--- webscraping.py
+++ webscraping.py
@@ -1,8 +1,6 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import selenium
-from selenium import webdriver
 
 my_url = "https://chycho.blogspot.com/"
 
@@ -12,9 +10,24 @@ homepage = request.read()
 
 homepageSoup = BeautifulSoup(homepage, features="lxml")
 
-posts = homepageSoup.findAll("div",{"class":"post hentry uncustomized-post-template"})
 
-print(len(posts))
-print(posts)
+nextPage = homepageSoup.find_all("a",{"class":"blog-pager-older-link"}, href=True)
+posts = homepageSoup.findAll("div",{"class":"post hentry uncustomized-post-template"})
+titles = homepageSoup.findAll("h3",{"class":"post-title entry-title"})
+dates = homepageSoup.findChild("h2",{"class":"date-header"})
+
+#for link in nextPage:
+print(nextPage[0]['href'])
+
+c = 0
+nextPage = homepageSoup.find_all("a",{"class":"blog-pager-older-link"}, href=True)
+while nextPage is not None:
+    c += 1
+    my_url = nextPage[0]['href']
+    request = urlopen(my_url)
+    homepage = request.read()
+    homepageSoup = BeautifulSoup(homepage, features="lxml")
+    nextPage = homepageSoup.find_all("a",{"class":"blog-pager-older-link"}, href=True)
+    print(nextPage[0]['href'])
 
 request.close()
