93135aa5aaebdb86959800620bf6989d1b80728a
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-08-19 20:31:40 -0300

    first dataset to test on mongoDB

--- webscrape.py
+++ webscrape.py
@@ -12,21 +12,22 @@ dates = homepageSoup.findAll("div",{"class":"date-outer"})
 
 def get_posts(dates, posts):
     for date in dates:
-        #print(date.find('h2',{"class":"date-header"}).text)
         page_posts = (date.findAll('div',{"class":"post-outer"}))
         for page_post in page_posts:
             post = {}
             #print(page_post.find('h3',{"class":"post-title entry-title"}).text) 
-            post['title'] = page_post.find('h3',{"class":"post-title entry-title"}).text
+            post['author'] = "chycho"
+            title = page_post.find('h3',{"class":"post-title entry-title"}).text
+            post['title'] = title.replace('\n','')
+            post['date'] = date.find('h2',{"class":"date-header"}).text
             post['body'] = page_post.find('div',{"class":"post-body entry-content"}).text
             links = page_post.findAll('strong')
             for link in links:
                 post['links'] = str(link.find('a',href=True))
-                #print(link.find('a',href=True))
             posts.append(post)
 
     print(posts)
-    #return posts
+    return posts
 
 nextPage = homepageSoup.find_all("a",{"class":"blog-pager-older-link"}, href=True)
 posts = []
@@ -38,8 +39,8 @@ while nextPage is not None:
     request = urlopen(my_url)
     homepage = request.read()
     homepageSoup = BeautifulSoup(homepage, "html.parser")
-    with open('data.json', 'a', encoding='utf-8') as f:
-        json.dump(posts, f, ensure_ascii=False, indent=4)
 
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(posts, f, ensure_ascii=False, indent=4)
 
 request.close()
